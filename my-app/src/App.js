08e613551cfc356da3c21f05ab56015e70a65bf6
Yuliia Burko <yuliia.burko@gmail.com>
2023-01-07 21:25:00 +0100

    scss styles

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,86 +1,83 @@
 import React from 'react';
-import './App.css';
-import {Card} from './components/card/Card'
+import './App.scss';
+import { Card } from './components/card/Card';
 
-
-class App extends React.Component{
+class App extends React.Component {
   constructor(props) {
-    super()
+    super();
     this.state = {
-  animals: [ { name: 'cat', color: 'pink'}, 
-             { name: 'dog', color: 'black'}, 
-             { name: 'tiger', color: 'orange'}, 
-             { name: 'fox', color: 'ginger'}, 
+      animals: [
+        { name: 'cat', color: 'pink' },
+        { name: 'dog', color: 'black' },
+        { name: 'tiger', color: 'orange' },
+        { name: 'fox', color: 'ginger' },
         { name: 'lion', color: 'yellow' },
-        { name: 'croco', color: 'green' }],
-      
-      title: "Animals",
+        { name: 'croco', color: 'green' },
+      ],
+
+      title: 'Animals',
       showCards: false,
-      showDescription:false,
-}
- }
-//При использовнии стрелояной функции не возникает проблем с потерей контекста!!!
-  inputHandler = (event) => {
+      showDescription: false,
+    };
+  }
+  //При использовнии стрелояной функции не возникает проблем с потерей контекста!!!
+  inputHandler = event => {
     console.log(event.target.value);
-    this.setState({title: event.target.value})
-}
+    this.setState({ title: event.target.value });
+  };
 
   eventHandler(title) {
-    console.log('work')
+    console.log('work');
     this.setState({
-        title
-    })
+      title,
+    });
   }
 
   showCardsHandler = () => {
-  this.setState({showCards: !this.state.showCards})
-}
-showDescrHandler = () => {
-  this.setState({showDescription: !this.state.showDescription})
-}
+    this.setState({ showCards: !this.state.showCards });
+  };
+  showDescrHandler = () => {
+    this.setState({ showDescription: !this.state.showDescription });
+  };
 
+  render() {
+    let animal = this.state.animals;
 
-render(){
+    return (
+      <div>
+        <div style={{ textAlign: 'center' }}>
+          <h1>{this.state.title}</h1>
+        </div>
 
-let animal = this.state.animals
+        {this.state.showCards ? (
+          <div className="App">
+            {animal.map((item, i) => {
+              return (
+                <Card
+                  name={item.name}
+                  color={item.color}
+                  click={this.eventHandler.bind(this)}
+                  key={i}
+                  show={this.state.showDescription}
+                />
+              );
+            })}
+          </div>
+        ) : null}
 
-  return (
-    <div>
-      <div style={{ textAlign: 'center' }}><h1>{this.state.title}</h1></div>
-      
-      {this.state.showCards ? <div className="App">
-        
-        {animal.map((item, i) => {
-          return (
-            <Card name={item.name} color={item.color} click={this.eventHandler.bind(this)} key={i} show={this.state.showDescription} />
-          )
-        })}</div> : null}
-
-      
-        
-
-
-  {/* <Card name = {animal[0].name} color = {animal[0].color} click={this.eventHandler.bind(this)}/>
+        {/* <Card name = {animal[0].name} color = {animal[0].color} click={this.eventHandler.bind(this)}/>
   <Card name = {animal[1].name} color = {animal[1].color} click={this.eventHandler.bind(this)}/>
   <Card name = {animal[2].name} color = {animal[2].color} click={this.eventHandler.bind(this)}/>
   <Card name = {animal[3].name} color = {animal[3].color} click={this.eventHandler.bind(this)}/>
   <Card name = {animal[4].name} color = {animal[4].color} click={this.eventHandler.bind(this)}/> */}
- 
 
-      <div>
-        <button onClick={this.showCardsHandler}>SHOW/HIDE</button>
-        <button onClick={this.showDescrHandler}>DESCRIPTION</button>
-         
+        <div>
+          <button onClick={this.showCardsHandler}>SHOW/HIDE</button>
+          <button onClick={this.showDescrHandler}>DESCRIPTION</button>
+        </div>
       </div>
-
-  </div>
-  
-)
-
+    );
+  }
 }
-
-
-}
-
 
 export default App;
--- my-app/src/components/card/Card.js
+++ my-app/src/components/card/Card.js
@@ -1,25 +1,21 @@
-import React from 'react'
-
-
-
-export const Card = (props) =>{
-
-    if (props.show) {
-        return(
-        <div style = {{minWidth: '200px', minHeight: '200px', textAlign: 'center', border: '1px solid black', padding: '10px', margin: '10px'}}>
-            <h2 onClick={() => props.click(props.name)}>Name: {props.name} </h2>
-                <h3>Color: {props.color}</h3>
-                <h3>Описание:</h3>
-                <p>Здесь будет описание животного</p>
-        </div>
-    )
-    }
-    else {
-         return(
-        <div style = {{minWidth: '200px', minHeight: '200px', textAlign: 'center', border: '1px solid black', padding: '10px', margin: '10px'}}>
-            <h2 onClick={() => props.click(props.name)}>Name: {props.name} </h2>
-            <h3>Color: {props.color}</h3>
-        </div>
-    )
-    }
-}
+import React from 'react';
+import classes from './Card.module.scss';
+export const Card = props => {
+  if (props.show) {
+    return (
+      <div className={classes.card}>
+        <h2 onClick={() => props.click(props.name)}>Name: {props.name} </h2>
+        <h3>Color: {props.color}</h3>
+        <h3>Описание:</h3>
+        <p>Здесь будет описание животного</p>
+      </div>
+    );
+  } else {
+    return (
+      <div className={classes.card}>
+        <h2 onClick={() => props.click(props.name)}>Name: {props.name} </h2>
+        <h3>Color: {props.color}</h3>
+      </div>
+    );
+  }
+};
